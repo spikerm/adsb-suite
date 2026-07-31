a90acb703bc6a2782ee0b39c7fa0307b68d2b95b
spikerm <rmarkesteijn79@gmail.com>
2026-07-31 11:58:48 +0200

    Add v3.1 intelligence module

---
+++ server/v31_intelligence.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Intelligence features for ADS-B Suite v3.1.
+
+This module is the integration point for the v3.1 intelligence layer.
+"""
+
+from __future__ import annotations
+
+VERSION = "3.1.0"
